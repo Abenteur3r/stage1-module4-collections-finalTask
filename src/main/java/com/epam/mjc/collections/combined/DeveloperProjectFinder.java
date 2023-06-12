8b54c1a2ccc20d8b9af438023a30069d922deb8d
Ayan <ajan.kohan@gmail.com>
2023-06-12 22:50:19 +0600

    final tasks

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -5,11 +5,9 @@ import java.util.*;
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
         List<String> res = new LinkedList<>();
-        for(String key : projects.keySet()){
-//            System.out.println(i.getKey()+"\t\t"+i.getValue());
-            if(projects.get(key).contains(developer)){
-                System.out.println(key);
-                res.add(key);
+        for(Map.Entry<String, Set<String>> i : projects.entrySet()){
+            if(i.getValue().toString().contains(developer)){
+                res.add(i.getKey());
             }
         }
         res.sort(new comp());
@@ -19,6 +17,10 @@ public class DeveloperProjectFinder {
 class comp implements Comparator<String>{
     @Override
     public int compare(String o1, String o2) {
+        if (o1.length() == o2.length()){
+            return -o1.compareToIgnoreCase(o2);
+
+        }
         return Integer.compare(o2.length(),o1.length());
     }
 }
